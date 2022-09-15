5c0ea65207216d94a971e05ce332d9d4860e9bf9
Swaron <swaronline0@gmail.com>
2022-09-15 13:58:44 +0300

    Updates

---
+++ precache-manifest.2b1a00bb912da4aac64730672354b377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3ab55dd3a6ac07a631b374e294055c",
+    "url": "/it-incubator-todolist-ts-01/index.html"
+  },
+  {
+    "revision": "024793b3b4dbafcb1e0f",
+    "url": "/it-incubator-todolist-ts-01/static/css/main.acc17dfc.chunk.css"
+  },
+  {
+    "revision": "364fb937578ae601be28",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.25bc4c52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.25bc4c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024793b3b4dbafcb1e0f",
+    "url": "/it-incubator-todolist-ts-01/static/js/main.0052a677.chunk.js"
+  },
+  {
+    "revision": "f66ab8bd731da1733e57",
+    "url": "/it-incubator-todolist-ts-01/static/js/runtime-main.fae40ad6.js"
+  }
+]);
